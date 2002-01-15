a9e7069264ef0bbd011307ebaad5c3cf52b69023
pkot <pkot>
2002-01-15 12:01:00 +0000

    SMS sending for AT mode. It does nt work yet, but putting it into CVS gives more opportunity for the code to get tested ;-) Fixes to the commit I made tonight that broke compile. Commits at 2 am are not A Good Thing (tm). Moved bin2hex and hex2bin functions to gsm-encoding.c. Cleanups.

--- include/gnokii/sms.h
+++ include/gnokii/sms.h
@@ -1,6 +1,6 @@
 /*
 
-  $Id: sms.h,v 1.15 2002-01-10 10:14:09 pkot Exp $
+  $Id: sms.h,v 1.16 2002-01-15 12:01:02 pkot Exp $
 
   G N O K I I
 
@@ -430,7 +430,6 @@ typedef struct {
 } GSM_CBMessage;
 
 /* Utils */
-extern void hex2bin(unsigned char *dest, const unsigned char *src, unsigned int len);
 extern char *GetBCDNumber(u8 *Number);
 extern int SemiOctetPack(char *Number, unsigned char *Output, SMS_NumberType type);
 extern SMS_DateTime *UnpackDateTime(u8 *Number, SMS_DateTime *dt);
