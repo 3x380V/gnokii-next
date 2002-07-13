9f18c1abaf8fd9b6b1fa0778b594cd1363c94a7b
pkot <pkot>
2002-07-13 22:22:44 +0000

    Output --getphonebook in more userfriendly manner; use -r or --raw for the old behaviour

--- common/phones/nk6510.c
+++ common/phones/nk6510.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: nk6510.c,v 1.39 2002-07-13 17:28:51 pkot Exp $
+  $Id: nk6510.c,v 1.40 2002-07-13 22:22:44 pkot Exp $
 
   G N O K I I
 
@@ -1067,7 +1067,6 @@ static GSM_Error P6510_IncomingPhonebook(int messagetype, unsigned char *message
 			case 0x30:
 				return GE_INVALIDMEMORYTYPE;
 			case 0x33:
-
 				return GE_EMPTYLOCATION;
 			case 0x34:
 				return GE_INVALIDLOCATION;
@@ -1149,7 +1148,7 @@ static GSM_Error P6510_IncomingPhonebook(int messagetype, unsigned char *message
 					DecodeUnicode(subEntry->data.Number, (blockstart + 10), blockstart[9] / 2);
 					if (!subblockcount) strcpy(data->PhonebookEntry->Number, subEntry->data.Number);
 					dprintf("   Type: %d (%02x)\n", subEntry->NumberType, subEntry->NumberType);
-					dprintf(" Number: %s\n", subEntry->data.Number);
+					dprintf("   Number: %s\n", subEntry->data.Number);
 					subblockcount++;
 					data->PhonebookEntry->SubEntriesCount++;
 				}
