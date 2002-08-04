8ebb7b70cf30f85d82bd20e1ea77ed81a2d3bf97
plail <plail>
2002-08-04 15:37:54 +0000

    change PNOK_FBUS_SendSMS to _EncodeSMS, change phone drivers accordingly

--- common/phones/nk6100.c
+++ common/phones/nk6100.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: nk6100.c,v 1.96 2002-07-29 21:29:48 bozo Exp $
+  $Id: nk6100.c,v 1.97 2002-08-04 15:37:54 plail Exp $
 
   G N O K I I
 
@@ -1179,7 +1179,10 @@ static GSM_Error SetCellBroadcast(GSM_Data *data, GSM_Statemachine *state)
 
 static GSM_Error SendSMSMessage(GSM_Data *data, GSM_Statemachine *state)
 {
+	unsigned char req[256] = {FBUS_FRAME_HEADER, 0x01, 0x02, 0x00};
 	GSM_Data dtemp;
+	GSM_Error error;
+	int len;
 
 	/*
 	 * FIXME:
@@ -1189,7 +1192,16 @@ static GSM_Error SendSMSMessage(GSM_Data *data, GSM_Statemachine *state)
 	GSM_DataClear(&dtemp);
 	GetNetworkInfo(&dtemp, state);
 
-	return PNOK_FBUS_SendSMS(data, state);
+
+	len = PNOK_FBUS_EncodeSMS(data, state, req + 6);
+	len += 6;
+
+	if (SM_SendMessage(state, len, PNOK_MSG_SMS, req) != GE_NONE) return GE_NOTREADY;
+	do {
+		error = SM_BlockNoRetryTimeout(state, data, PNOK_MSG_SMS, state->Link.SMSTimeout);
+	} while (!state->Link.SMSTimeout && error == GE_TIMEOUT);
+
+	return error;
 }
 
 static bool CheckIncomingSMS(GSM_Statemachine *state, int pos)
