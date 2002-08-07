0e1c510e148d0228b0da4d8b178b724883ad3b11
plail <plail>
2002-08-07 10:43:45 +0000

    phonebook: name can be assigned to both bitmap and phonebook entry

--- common/nokia-decoding.c
+++ common/nokia-decoding.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: nokia-decoding.c,v 1.3 2002-08-04 20:35:14 pkot Exp $
+  $Id: nokia-decoding.c,v 1.4 2002-08-07 10:43:45 plail Exp $
 
   G N O K I I
 
@@ -79,8 +79,9 @@ GSM_Error DecodePhonebook(unsigned char *blockstart, int length, GSM_Data *data,
 		case PNOKIA_ENTRYTYPE_NAME:	/* Name */
 			if (data->Bitmap) {
 				DecodeUnicode(data->Bitmap->text, (blockstart + 6), blockstart[5] / 2);
-				dprintf("Name: %s\n", data->Bitmap->text);
-			} else if (data->PhonebookEntry) {
+				dprintf("Bitmap Name: %s\n", data->Bitmap->text);
+			}
+			if (data->PhonebookEntry) {
 				DecodeUnicode(data->PhonebookEntry->Name, (blockstart + 6), blockstart[5] / 2);
 				data->PhonebookEntry->Empty = false;
 				dprintf("   Name: %s\n", data->PhonebookEntry->Name);
