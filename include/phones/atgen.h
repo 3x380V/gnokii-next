eb1035198b93354300b90e1eb5ae7b2bffe04188
ladis <ladis>
2003-01-05 20:47:23 +0000

    don't missuse model for reading charset

--- include/phones/atgen.h
+++ include/phones/atgen.h
@@ -1,6 +1,6 @@
 /*
 
-  $Id: atgen.h,v 1.13 2002-12-09 14:45:53 ladis Exp $
+  $Id: atgen.h,v 1.14 2003-01-05 20:47:23 ladis Exp $
 
   G N O K I I
 
@@ -24,7 +24,7 @@
 
   Copyright 2001 Manfred Jonsson <manfred.jonsson@gmx.de>
 
-  This file provides functions specific to generic at command compatible
+  This file provides functions specific to generic AT command compatible
   phones. See README for more details on supported mobile phones.
 
 */
@@ -67,6 +67,7 @@ typedef struct {
 	gn_memory_type smsmemorytype;
 	at_charset defaultcharset;
 	at_charset charset;
+	char *charsetstr;
 } at_driver_instance;
 
 #define AT_DRVINST(s) ((at_driver_instance *)((s)->driver.driver_instance))
