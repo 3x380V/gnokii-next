a2b7535af8acf19d33120db3d9f4985a50e602a0
pkot <pkot>
2003-09-22 19:29:04 +0000

    Bump the version number for win32 port

--- win32/MSVC6/config.h
+++ win32/MSVC6/config.h
@@ -1,6 +1,6 @@
 /*
 
-  $Id: config.h,v 1.7 2003-04-22 17:12:25 ladis Exp $
+  $Id: config.h,v 1.8 2003-09-22 19:29:04 pkot Exp $
 
   G N O K I I
 
@@ -40,7 +40,7 @@
 
 	
 #undef VERSION
-#define VERSION "0.5.0"
+#define VERSION "0.5.4"
 
 /* No bluetooth and no IRDA */
 #undef HAVE_BLUETOOTH
