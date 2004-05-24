2a9bc3823e171911e37a9f71d8b82cf0e0f1cebe
bozo <bozo>
2004-05-24 15:54:29 +0000

    added Nokia 3595 to the supported models

--- common/phones/nk6510.c
+++ common/phones/nk6510.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: nk6510.c,v 1.144 2004-04-25 16:00:04 pkot Exp $
+  $Id: nk6510.c,v 1.145 2004-05-24 15:54:43 bozo Exp $
 
   G N O K I I
 
@@ -213,7 +213,7 @@ gn_driver driver_nokia_6510 = {
 	pgen_incoming_default,
 	/* Mobile phone information */
 	{
-		"6510|6310|8310|6310i|6360|6610|6100|5100|3510|3510i", /* Supported models */
+		"6510|6310|8310|6310i|6360|6610|6100|5100|3510|3510i|3595", /* Supported models */
 		7,                     /* Max RF Level */
 		0,                     /* Min RF Level */
 		GN_RF_Percentage,      /* RF level units */
