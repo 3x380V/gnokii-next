01b2c2812415e66c888190a1ce8c7a4f4f794e4f
pkot <pkot>
2005-02-06 18:52:46 +0000

    Fix printing year > 2004 in the debug output for SMS

--- common/gsm-sms.c
+++ common/gsm-sms.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: gsm-sms.c,v 1.143 2004-05-08 20:52:39 bozo Exp $
+  $Id: gsm-sms.c,v 1.144 2005-02-06 18:52:46 pkot Exp $
 
   G N O K I I
 
@@ -146,7 +146,7 @@ static char *sms_timestamp_print(u8 *number)
 
 	/* Ugly hack, but according to the GSM specs, the year is stored
          * as the 2 digit number. */
-	if (number[0] < 70) sprintf(buffer, "20");
+	if ((10*(number[0] & 0x0f) + number[0] >> 4) < 70) sprintf(buffer, "20");
 	else sprintf(buffer, "19");
 
 	sprintf(buffer, "%s%d%d-", buffer, number[0] & 0x0f, number[0] >> 4);
