506f41737fa7d545738b947d873ecab468d42722
pkot <pkot>
2005-04-28 20:35:58 +0000

    Fix off-by-one bug in xgnokii sms number handling; submitted by anonymous, bug\#12827

--- xgnokii/xgnokii_sms.c
+++ xgnokii/xgnokii_sms.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: xgnokii_sms.c,v 1.59 2005-04-24 20:57:37 pkot Exp $
+  $Id: xgnokii_sms.c,v 1.60 2005-04-28 20:35:59 pkot Exp $
 
   X G N O K I I
 
@@ -1138,8 +1138,8 @@ static void DoSendSMS(void)
 			sms.delivery_report = false;
 		sms.type = GN_SMS_MT_Submit;
 
-		strncpy(sms.remote.number, number, GN_BCD_STRING_MAX_LENGTH + 1);
-		sms.remote.number[GN_BCD_STRING_MAX_LENGTH] = '\0';
+		strncpy(sms.remote.number, number, GN_BCD_STRING_MAX_LENGTH);
+		sms.remote.number[GN_BCD_STRING_MAX_LENGTH - 1] = '\0';
 		if (sms.remote.number[0] == '+')
 			sms.remote.type = GN_GSM_NUMBER_International;
 		else 
