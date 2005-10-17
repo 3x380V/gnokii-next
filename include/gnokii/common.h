097cad1eb7223f44faedbfbee87913511b61b35b
bozo <bozo>
2005-10-17 23:26:27 +0000

    allow more subentries for a pbk entry

--- include/gnokii/common.h
+++ include/gnokii/common.h
@@ -1,6 +1,6 @@
 /*
 
-  $Id: common.h,v 1.137 2005-08-23 22:11:04 pkot Exp $
+  $Id: common.h,v 1.138 2005-10-17 23:26:27 bozo Exp $
 
   G N O K I I
 
@@ -137,7 +137,7 @@ typedef struct {
    not support these lengths so they have their own limits set. */
 #define GN_PHONEBOOK_NAME_MAX_LENGTH            61   /* For 6510 */
 #define GN_PHONEBOOK_NUMBER_MAX_LENGTH          49   /* For 6510 */
-#define GN_PHONEBOOK_SUBENTRIES_MAX_NUMBER      10   /* For 6510 */
+#define GN_PHONEBOOK_SUBENTRIES_MAX_NUMBER      20   /* it was 10 for 6510, but modern phones supports more */
 						     /* 7110 is able to have in one
 						      * entry 5 numbers and 4
 						      * texts [email,notice,postal,url] */
