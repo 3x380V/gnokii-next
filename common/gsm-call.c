5279a95508bf22e905faa8148807ee861c775b18
dforsi <dforsi>
2006-07-05 21:51:46 +0000

    use GN_OP_CancelCall in gn_call_cancel()

--- common/gsm-call.c
+++ common/gsm-call.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: gsm-call.c,v 1.14 2006-06-11 16:39:48 deller Exp $
+  $Id: gsm-call.c,v 1.15 2006-07-05 21:51:46 dforsi Exp $
 
   G N O K I I
 
@@ -168,7 +168,7 @@ GNOKII_API gn_error gn_call_cancel(int call_id)
 	gn_data_clear(&data);
 	data.call_info = &call_info;
 
-	return gn_sm_functions(GN_OP_AnswerCall, &data, calltable[call_id].state);
+	return gn_sm_functions(GN_OP_CancelCall, &data, calltable[call_id].state);
 }
 
 GNOKII_API gn_call *gn_call_get_active(int call_id)
