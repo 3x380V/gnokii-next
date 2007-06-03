03ba55610e77716d71c359fbaf909e8798b8f9f7
pkot <pkot>
2007-06-03 19:46:19 +0000

    This release changes libgnokii API

--- include/gnokii.h
+++ include/gnokii.h
@@ -1,6 +1,6 @@
 /*
 
-  $Id: gnokii.h,v 1.108 2006-11-26 16:24:24 pkot Exp $
+  $Id: gnokii.h,v 1.109 2007-06-03 19:46:19 pkot Exp $
 
   G N O K I I
 
@@ -65,9 +65,9 @@ typedef unsigned char uint8_t;
 #endif
 
 /* if you change those numbers, change them in common/Makefile as well! */
-#define LIBGNOKII_VERSION_STRING "4.1.0"
-#define LIBGNOKII_VERSION_MAJOR 4
-#define LIBGNOKII_VERSION_MINOR 1
+#define LIBGNOKII_VERSION_STRING "5.3.0"
+#define LIBGNOKII_VERSION_MAJOR 5
+#define LIBGNOKII_VERSION_MINOR 2
 #define LIBGNOKII_VERSION_RELEASE 0
 #define LIBGNOKII_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
 
