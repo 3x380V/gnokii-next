6038ef495ba5eeba21df0f2e07ba2c613cc3c890
dforsi <dforsi>
2007-06-12 16:00:32 +0000

    don't return after reading an empty name (meaning this is a request to delete the entry) because memory type and location are needed too

--- common/gsm-filetypes.c
+++ common/gsm-filetypes.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: gsm-filetypes.c,v 1.73 2007-05-22 15:19:38 dforsi Exp $
+  $Id: gsm-filetypes.c,v 1.74 2007-06-12 16:00:32 dforsi Exp $
 
   G N O K I I
 
@@ -1464,7 +1464,7 @@ GNOKII_API gn_error gn_file_phonebook_raw_parse(gn_phonebook_entry *entry, char 
 		return GN_ERR_WRONGDATAFORMAT;
 	case 1:
 		/* empty name: this is a request to delete the entry */
-		return GN_ERR_NONE;
+		break;
 	default:
 		break;
 	}
