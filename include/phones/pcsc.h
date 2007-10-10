6ae649e56c09896707782407e409f5f7a77aa9b8
dforsi <dforsi>
2007-10-10 20:57:09 +0000

    add support for LD memory

--- include/phones/pcsc.h
+++ include/phones/pcsc.h
@@ -1,6 +1,6 @@
 /*
 
-  $Id: pcsc.h,v 1.2 2007-10-07 21:31:48 dforsi Exp $
+  $Id: pcsc.h,v 1.3 2007-10-10 20:57:10 dforsi Exp $
 
   G N O K I I
 
@@ -63,7 +63,7 @@ subclause : command
 
 #define GN_PCSC_FILE_STRUCTURE_TRANSPARENT 0
 #define GN_PCSC_FILE_STRUCTURE_LINEAR_FIXED 1
-#define GN_PCSC_FILE_STRUCTURE_CYCLIC 2
+#define GN_PCSC_FILE_STRUCTURE_CYCLIC 3
 
 #define GN_PCSC_FILE_TYPE_MF 1
 #define GN_PCSC_FILE_TYPE_DF 2
@@ -87,6 +87,8 @@ subclause : command
 #define GN_PCSC_FILE_EF_SMS 0x6F3C
 /* Mobile Subscriber Integrated Services Digital Network Number */
 #define GN_PCSC_FILE_EF_MSISDN 0x6F40
+/* Last Numbers Dialed */
+#define GN_PCSC_FILE_EF_LND 0x6F44
 /* Service Dialing Numbers */
 #define GN_PCSC_FILE_EF_SDN 0x6F49
 /* Barred Dialing Numbers */
