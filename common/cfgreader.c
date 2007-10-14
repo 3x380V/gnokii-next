4e1ab09e0b086ae7a90637dc77cc6443dea7e00a
dforsi <dforsi>
2007-10-14 14:37:57 +0000

    return value for cfg_psection_load() is a gn_error now

--- common/cfgreader.c
+++ common/cfgreader.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: cfgreader.c,v 1.81 2007-10-13 12:33:17 pkot Exp $
+  $Id: cfgreader.c,v 1.82 2007-10-14 14:37:57 dforsi Exp $
 
   G N O K I I
 
@@ -700,13 +700,14 @@ GNOKII_API gn_error gn_cfg_memory_read(const char **lines)
 GNOKII_API gn_error gn_cfg_phone_load(const char *iname, struct gn_statemachine *state)
 {
 	char section[256];
+	gn_error error;
 
 	if (iname == NULL || *iname == '\0') {
 		state->config = gn_config_global;
 	} else {
 		snprintf(section, sizeof(section), "phone_%s", iname);
-		if (!cfg_psection_load(&state->config, section, &gn_config_global))
-			return GN_ERR_NOPHONE;
+		if ((error = cfg_psection_load(&state->config, section, &gn_config_global)) != GN_ERR_NONE)
+			return error;
 	}
 
 	if (state->config.model[0] == '\0') {
