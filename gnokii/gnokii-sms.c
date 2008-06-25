9de20d8236f071ad55e632dd44ca48f8e8bfe529
dforsi <dforsi>
2008-06-25 18:13:51 +0000

    fix handling of arguments in --savesms: * -l|--location wasn't accepted * increase number of allowed parameters from 10 to 12 so that this will work:   2# --sender from   2# [--smsc message_center_number | --smscno message_center_index]   2# [-f|--folder folder_id]   2# [-l|--location number]   1# [-s|--sent|-r|--read]   1# [-d|--deliver]   2# [-t|--datetime YYMMDDHHMMSS]

--- gnokii/gnokii-sms.c
+++ gnokii/gnokii-sms.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: gnokii-sms.c,v 1.49 2008-06-21 09:23:54 dforsi Exp $
+  $Id: gnokii-sms.c,v 1.50 2008-06-25 18:13:51 dforsi Exp $
 
   G N O K I I
 
@@ -464,7 +464,7 @@ gn_error savesms(int argc, char *argv[], gn_data *data, struct gn_statemachine *
 				sms.remote.type = GN_GSM_NUMBER_Unknown;
 			snprintf(sms.remote.number, GN_BCD_STRING_MAX_LENGTH, "%s", optarg);
 			break;
-		case '3': /* location to write to */
+		case 'l': /* location to write to */
 			sms.number = gnokii_atoi(optarg);
 			if (errno || sms.number < 0)
 				savesms_usage(stderr, -1);
--- gnokii/gnokii.c
+++ gnokii/gnokii.c
@@ -1,6 +1,6 @@
 /*
 
-  $Id: gnokii.c,v 1.485 2008-05-31 19:09:46 dforsi Exp $
+  $Id: gnokii.c,v 1.486 2008-06-25 18:13:51 dforsi Exp $
 
   G N O K I I
 
@@ -664,7 +664,7 @@ static int parse_options(int argc, char *argv[])
 		{ OPT_GETSMS,            2, 6, 0 },
 		{ OPT_DELETESMS,         2, 3, 0 },
 		{ OPT_SENDSMS,           1, 10, 0 },
-		{ OPT_SAVESMS,           0, 10, 0 },
+		{ OPT_SAVESMS,           0, 12, 0 },
 		{ OPT_SENDLOGO,          3, 4, GAL_XOR },
 		{ OPT_SENDRINGTONE,      2, 2, 0 },
 		{ OPT_GETSMSC,           0, 3, 0 },
